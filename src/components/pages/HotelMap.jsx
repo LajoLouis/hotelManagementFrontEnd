40afa72676a2ea1ea982623637f10600e04f8dc0
lajo <ezeokefranklin@gmail.com>
2024-12-03 20:30:22 +0100

    hotel map changes2

--- src/components/pages/HotelMap.jsx
+++ src/components/pages/HotelMap.jsx
@@ -76,7 +76,7 @@ function HotelMap({specificHotelLocation, specificHotelName, fullPage}) {
     };
 
   return (
-    <MapContainer center={center} zoom={13} className={`${fullPage ? "h-full" : "h-[200px]"}`}>
+    <MapContainer center={center} zoom={13} className={`${fullPage ? "h-screen" : "h-[200px]"}`}>
         <TileLayer
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
