8a8686731efd8d51f1981db4eb12db49396e41c3
lajo <ezeokefranklin@gmail.com>
2024-12-04 14:03:14 +0100

    booking payment reroute1

--- src/components/pages/BookRoom.jsx
+++ src/components/pages/BookRoom.jsx
@@ -46,6 +46,7 @@ function BookRoom({ hotelName, bookedRoom }) {
         showAndHide("error", "Please Input checkIn and checkOut date")
       }else if (data === "Unauthorized access") {
         showAndHide("error", "Kindly Login")
+        console.log("unauthorized access");
         
       }else if (data === "please select a room") {
         showAndHide("error", "Kindly select a room")
